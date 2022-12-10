7991d598541ae4fb61896ed1c938bbf0eeaca5db
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-11 01:52:29 +0200

    add delete contact

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,14 +1,15 @@
 // import PropTypes from 'prop-types';
 
-// import { ControlsBox, Button } from './FeedbackOptions.styled';
+import { Contact } from 'components';
 
-export const ContactList = ({ items }) => {
+export const ContactList = ({ items, onDelete }) => {
+  console.log(items);
   return (
     <ul>
       {items.map(item => {
         return (
           <li key={item.id}>
-            <p item={item}></p>
+            <Contact contact={item} onDelete={onDelete}></Contact>
           </li>
         );
       })}
