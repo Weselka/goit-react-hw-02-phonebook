f7d7d62ce9990be678a6eff672fdacf09824a649
Vasylyna Shvets <104644733+Weselka@users.noreply.github.com>
2022-12-10 15:17:23 +0200

    add begin file

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Heading } from 'components';
+
+export const App = () => {
+  return (
+    <>
+      <Heading marginBottom="50px" textAlign="center">
+        Phonebook
+      </Heading>
+      {/* < /> */}
+    </>
+  );
+};
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export * from 'components/App/App';
+export * from 'components/Container/Container.styled.js';
+export * from 'components/Section/Section.js';
+export * from 'components/Section/Section.styled.js';
+export * from 'components/Heading/Heading.styled.js';
